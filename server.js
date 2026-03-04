c37136bf515ca4db29de3d1f665754749b9fd0fa
mathankumar8248097868-cloud <mathankumar8248097868@gmail.com>
2026-03-04 14:56:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
-app.listen(5000, () => {
-  console.log("Server running at http://localhost:5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
